806b71f8a82d17915c68206461da0bfe4c7e2003
Luan Gong <luan.gong@gmail.com>
2016-10-30 23:02:19 -0700

    Add a template

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-    return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a>')
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{boldmessage}} in the template!
+    context = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context)
 
 def about(request):
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
     return HttpResponse('Rango says here is the about page. <a href="/rango/">Home Page</a>')
