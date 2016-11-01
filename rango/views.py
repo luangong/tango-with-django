e882f1541025551852edc2ada1570cc6d3496e3e
Luan Gong <luan.gong@gmail.com>
2016-10-31 22:48:14 -0700

    Convert the "about" page to use a template as well

--- rango/views.py
+++ rango/views.py
@@ -12,4 +12,5 @@ def about(request):
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return HttpResponse('Rango says here is the about page. <a href="/rango/">Home Page</a>')
+    context = {'name': 'Luan Gong'}
+    return render(request, 'rango/about.html', context)
